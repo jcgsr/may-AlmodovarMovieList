760cca12ac6590eddf37e5fa6d426997f9da22ad
jcgsr <cabralguerra@gmail.com>
2022-05-14 19:21:47 -0300

    react helmet

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 
+import { Helmet } from "react-helmet";
+
 import api from "../../services/api";
 
 import { Link, animateScroll as scroll } from "react-scroll";
@@ -41,94 +43,121 @@ export default function Home() {
   }
 
   return (
-    <Container>
-      <h1>Almodóvar Cult Movie List</h1>
-
-      <Form className="d-flex">
-        <FormControl
-          type="search"
-          placeholder="Filme"
-          className="mr-2"
-          autoFocus
-          value={searchFilm}
-          id="search_movie"
-          aria-label="Search"
-          onChange={(event) => {
-            setSearchFilm(event.target.value);
-          }}
+    <>
+      <Helmet>
+        <title>Almodóvar Cult Movies List</title>
+        <meta
+          name="description"
+          content="Site com os melhores filmes considerados cult por meu grande amigo, Almodóvar."
         />
-      </Form>
-      <Row className="ctn">
-        <Col>
-          {Object.values(filmes)
-            .filter((val) => {
-              if (searchFilm == "") {
-                return val;
-              } else if (
-                val.filme.toLowerCase().includes(searchFilm.toLowerCase())
-              ) {
-                return val;
-              }
-            })
-            .map((filme) => (
-              <li style={{ listStyle: "none" }} key={filme.filme}>
-                <h2 className="mt-4">{filme.filme}</h2>
-                <img
-                  data-aos="zoom-in"
-                  className="mb-2 animate__animated animate__pulse"
-                  src={filme.foto}
-                />
-                <br />
-                <span style={{ color: "#ff0" }}>diretor: </span>
-                {filme.diretor}
-                <section className="mt-4">
-                  <p className="sinopse" data-aos="zoom-in">
-                    <span style={{ color: "#ff0" }}>sinopse: </span>
-                    {filme.sinopse}
-                  </p>
-                </section>{" "}
-                <div className="mb-4">
-                  <a
-                    className=""
-                    target="blank"
-                    href={`https://youtube.com/results?search_query=${filme.filme} Trailer`}
-                  >
-                    <Button variant="info" size="lg">
-                      Trailer
-                    </Button>
-                  </a>
-                </div>
-                <hr style={{ color: "#ff0" }} />
-              </li>
-            ))}
-          {searchFilm !== filmes && (
-            <div className="no-movie">
-              <h2 className="mt-4 mb-4">{searchFilm}</h2>
-              <h3>não está nessa lista?! 😲</h3>
-              <h3>Deseja incluí-lo?!</h3>
-              <h4>Entre em contato pelo</h4>
-              <a href={whatsapp}>
-                <Button className="ml-2" size="lg">
-                  <FaWhatsapp size={24} />
-                </Button>
-              </a>
-            </div>
-          )}
+        <meta
+          property="og:url"
+          content="https://almodovarcultmovielist.netlify.app/"
+        />
 
-          <Button className="search-icon" variant="secondary">
-            <Link
-              activeClass="active"
-              to="search_movie"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-            >
-              <BsSearch size={24} />
-            </Link>
-          </Button>
-        </Col>
-      </Row>
-    </Container>
+        <meta property="og:title" content="Almodóvar Cult Movies List" />
+        <meta
+          property="og:description"
+          content="Site com os melhores filmes considerados cult por meu grande amigo, Almodóvar"
+        />
+        <meta
+          property="og:image"
+          content="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1QijCmZ8Ub_qNxmlsEsZfHclth_GyYi3m9w&usqp=CAU"
+        />
+        <meta
+          name="google-site-verification"
+          content="Yz87tJxDTnQXcRHF8NGlNzfmDVxJZ3vr8w_1e3v4Je4"
+        />
+      </Helmet>
+      <Container>
+        <h1>Almodóvar Cult Movies List</h1>
+
+        <Form className="d-flex">
+          <FormControl
+            type="search"
+            placeholder="Filme"
+            className="mr-2"
+            autoFocus
+            value={searchFilm}
+            id="search_movie"
+            aria-label="Search"
+            onChange={(event) => {
+              setSearchFilm(event.target.value);
+            }}
+          />
+        </Form>
+        <Row className="ctn">
+          <Col>
+            {Object.values(filmes)
+              .filter((val) => {
+                if (searchFilm == "") {
+                  return val;
+                } else if (
+                  val.filme.toLowerCase().includes(searchFilm.toLowerCase())
+                ) {
+                  return val;
+                }
+              })
+              .map((filme) => (
+                <li style={{ listStyle: "none" }} key={filme.filme}>
+                  <h2 className="mt-4">{filme.filme}</h2>
+                  <img
+                    data-aos="zoom-in"
+                    className="mb-2 animate__animated animate__pulse"
+                    src={filme.foto}
+                  />
+                  <br />
+                  <span style={{ color: "#ff0" }}>diretor: </span>
+                  {filme.diretor}
+                  <section className="mt-4">
+                    <p className="sinopse" data-aos="zoom-in">
+                      <span style={{ color: "#ff0" }}>sinopse: </span>
+                      {filme.sinopse}
+                    </p>
+                  </section>{" "}
+                  <div className="mb-4">
+                    <a
+                      className=""
+                      target="blank"
+                      href={`https://youtube.com/results?search_query=${filme.filme} Trailer`}
+                    >
+                      <Button variant="info" size="lg">
+                        Trailer
+                      </Button>
+                    </a>
+                  </div>
+                  <hr style={{ color: "#ff0" }} />
+                </li>
+              ))}
+            {searchFilm !== filmes && (
+              <div className="no-movie">
+                <h2 className="mt-4 mb-4">{searchFilm}</h2>
+                <h3>não está nessa lista?! 😲</h3>
+                <h3>Deseja incluí-lo?!</h3>
+                <h4>Entre em contato pelo</h4>
+                <a href={whatsapp}>
+                  <Button className="ml-2" size="lg">
+                    <FaWhatsapp size={24} />
+                  </Button>
+                </a>
+              </div>
+            )}
+
+            <Button className="search-icon" variant="secondary">
+              <Link
+                activeClass="active"
+                to="search_movie"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+                <BsSearch size={24} />
+              </Link>
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
